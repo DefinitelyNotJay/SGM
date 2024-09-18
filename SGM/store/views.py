f3ce5ae77212afb0a92e4af699beda7edff75947
aidefjs <aina-123@hotmail.co.th>
2024-09-18 22:39:49 +0700

    view-product

--- SGM/store/views.py
+++ SGM/store/views.py
@@ -8,3 +8,8 @@ class Inventory(View):
     def get(self, request):
         print(Customer.objects.all())
         return HttpResponse("123")
+
+class ViewStock(View):
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all()  # ดึงสินค้าทั้งหมด
+        return render(request, 'index.html', {'products': products})
