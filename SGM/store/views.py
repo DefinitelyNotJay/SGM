899117c042200e3e64dbdcfb1844ea9b367b1b02
DefinitelyNotJay <jjbrathers3@gmail.com>
2024-09-19 19:27:25 +0700

    edit customer success

--- SGM/store/views.py
+++ SGM/store/views.py
@@ -9,7 +9,7 @@ from django.forms.models import model_to_dict
 
 class Inventory(View):
     def get(self, request):
-        return render(request, "employee/add_customer.html", {"form": CustomerCreateForm()})
+        return render(request, "employee/customer_form.html", {"form": CustomerCreateForm()})
 
 class SignUp(View):
     def get(self, request):
@@ -22,22 +22,32 @@ class ManageCustomer(View):
             customer_instance = Customer.objects.get(pk=customer_id)
             edit_form = CustomerCreateForm(initial=model_to_dict(customer_instance), instance=customer_instance)
             context = {"form": edit_form, "customer": customer_instance}
-            return render(request, "employee/add_customer.html", context)
+            return render(request, "employee/customer_form.html", context)
 
         # get all customers
         customers = Customer.objects.all()
-        form = CustomerCreateForm()
-        print(form.fields.get("gender"))
-        context = {"form": form}
-        return render(request, "employee/add_customer.html", context)
+        return render(request, "employee/all_customer.html", {"customers": customers})
 
-    def post(self, request):
+    def post(self, request, customer_id=None):
+        if customer_id:
+            customer_instance = Customer.objects.get(pk=customer_id)
+            form = CustomerCreateForm(request.POST, instance=customer_instance)
+            if form.is_valid:
+                try:
+                    form.save()
+                    return redirect("/customer")
+                except:
+                    return HttpResponseServerError()
         # create customer
         form = CustomerCreateForm(request.POST)
         if form.is_valid:
             try:
                 customer = form.save()
-                return redirect("/")
+                return redirect("/customer")
             except:
                 return redirect("/customer/new/")
         return redirect("/customer")
+
+    def delete(self, request, customer_id):
+        print(customer_id)
+        return JsonResponse({"success": True})
