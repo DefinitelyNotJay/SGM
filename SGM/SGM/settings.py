a543c94bf5897d5311c6c9d0349504310d254399
Nutnicha Feuangfu <52133619+aidefjs@users.noreply.github.com>
2024-10-17 04:11:38 +0700

    Update settings.py

--- SGM/SGM/settings.py
+++ SGM/SGM/settings.py
@@ -134,6 +134,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'nutnichafeuangfu@gmail.com'
-EMAIL_HOST_PASSWORD = 'vcyj ttid lkiv mwmd'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
