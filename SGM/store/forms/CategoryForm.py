65ea451c85f905188d104afc7ed46a4b08f40091
aidefjs <aina-123@hotmail.co.th>
2024-10-17 15:52:18 +0700

    category

--- SGM/store/forms/CategoryForm.py
+++ SGM/store/forms/CategoryForm.py
@@ -1,5 +1,5 @@
-# forms.py
 from django import forms
+from django.core.exceptions import ValidationError
 from store.models import Category
 
 class CategoryForm(forms.ModelForm):
@@ -10,6 +10,16 @@ class CategoryForm(forms.ModelForm):
             'placeholder': 'กรุณากรอกชื่อหมวดหมู่'
         })
     )
+    
+    def clean_name(self):
+        name = self.cleaned_data.get('name')
+        
+        # ตรวจสอบว่าชื่อหมวดหมู่มีอยู่ในฐานข้อมูลหรือไม่
+        if Category.objects.filter(name=name).exists():
+            self.add_error("name", 'ชื่อหมวดหมู่นี้มีอยู่แล้ว กรุณากรอกชื่อใหม่')
+        
+        return name  # ส่งค่าที่ผ่านการตรวจสอบแล้วกลับไป
+    
     class Meta:
         model = Category
         fields = ['name']  # ชื่อหมวดหมู่ที่คุณต้องการเพิ่ม
